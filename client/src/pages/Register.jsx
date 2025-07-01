db9d162ebf66f69356b1b8515992673cbeddea1d
Aaron Sierp <aaronsierp@gmail.com>
2025-07-01 12:38:30 -0400

    built out registration page and linked to login page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,41 @@
+
+import { Navigate } from "react-router-dom";
+import { useState } from 'react';
+import Btn from "../components/Button";
+
+
 function Register() {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [confirmPassword, setConfirmPassword] = useState('');
+const [error, setError] = useState('')
+
+const submission =  async (e) => {
+  e.preventDefault();
+  setError('')
+}
+
   return (
-    <div>
-      <h2>Register</h2>
-    </div>
+   <div className = "container-register">
+      <h2>Registation Form</h2>
+    <form onSubmit={submission}>
+      <div>
+        <label>Email</label>
+        <input type = 'email' required value = {email} onChange = {(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type = "password" required value = {password} onChange = {(e) => setPassword(e.target.vlaue)} />
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input type = "confirmPassword" required value = {confirmPassword} onChange = {(e) => setConfirmPassword(e.target.value)} />
+      </div>
+      <Btn type="submit" name ="Register"/>
+    </form>
+   </div>
+    
   );
 }
 
