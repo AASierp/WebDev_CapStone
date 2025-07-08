10a2fd776a30e3a3cebdacd088b0c3f356cdbeba
Aaron Sierp <aaronsierp@gmail.com>
2025-07-08 11:12:33 -0400

    spun up express server on backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const db = require('../db');
+import express from 'express';
+import bcrypt from 'bcrypt';
+import db from '../db.js'
+
+// const express = require('express');
+// const bcrypt = require('bcrypt');
+// const db = require('../db');
 
 const router = express.Router();
 
@@ -13,36 +17,38 @@ router.post('/register', async (req, res) => {
     //setting the email and password sent in the request body to constant variables.
     const email = req.body.email;
     const password = req.body.password;
+
+
+    try {
+
+        //hashing the password before being sent to the database.
+        const hash = await bcrypt.hash(password, 13);
+
+        //using a 'prepared statement' here to prevent sql injection - 
+        //basically predefining the sql query and setting the values as parameters so they cant be manipulated fomr the outside
+
+        const dbInsert = db.prepare('INSERT INTO users(email, password) VALUES( ?, ?)');
+        //inserts the email and hashed password into the query and runs
+        dbInsert.run(email, hash);
+
+        // just sending a response if successful
+        res.status(201)
+        res.send('Database Insertion Successful');
+    } 
+    catch (error) {
+        //response if not successful - in this particular case there is a 'UNIQUE'(prevents duplication) constaint on the email address.
+        if(error.code === 'SQLITE_CONSTRAINT'){
+            res.status(400)
+            res.send('An Account With This Email Already Exists')
+        }
+        else{
+            console.error(error)
+            res.status(500);
+            res.send('Server Unavailable');
+        }
+    }
 });
 
-try {
-
-    //hashing the password before being sent to the database.
-    const hash = await bcrypt.hash(password, 13);
-
-    //using a 'prepared statement' here to prevent sql injection - 
-    //basically predefining the sql query and setting the values as parameters so they cant be manipulated fomr the outside
-
-    const dbInsert = db.prepare('INSERT INTO users(email, password) VALUES( ?, ?)');
-    //inserts the email and hashed password into the query and runs
-    dbinsert.run(email, hash);
-
-    // just sending a response if successful
-    res.status(201)
-    res.send('Database Insertion Successful');
-} 
-catch (error) {
-    //response if not successful - in this particular case there is a 'UNIQUE'(prevents duplication) constaint on the email address.
-    if(error.code === 'SQLITE_CONSTRAINT'){
-        res.status(400)
-        res.send('An Account With This Email Already Exists')
-    }
-    else{
-        console.error(error)
-        res.status(500);
-        res.send('Server Unavailable');
-    }
-}
 // exports to make avaible to the rest of the application
-module.exports = router;
+export default router;
 
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth.js';
+import db from "./db.js";
+
+// const express = require('express');
+// const cors = require('cors');
+// const authRoutes = require('./routes/auth')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Server Active');
+}); 
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`)
+});
